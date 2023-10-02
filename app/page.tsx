24dac2a0781f2116f71a78ca6789059a0015b84f
germainsafari <GG@ee10725997111>
2023-10-02 20:20:04 +0200

    tch

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import Hero from Component
+import Hero from "../components";
 import { Component } from "react";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Hero/>
+      <Hero />
     </main>
   );
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero</div>
+    <div>Hero </div>
   )
 }
 
