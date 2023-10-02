254b17ed173a623baac485e3cc7a88ddc9c5e277
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-02 20:27:11 +0200

    tch

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>Hero </div>
+    <div>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className='hero__title'>
+                Find, book, or rent a car
+            </h1>
+        </div>
+        <p>
+            streamline your car rental experience
+        </p>
+    </div>
   )
 }
 
