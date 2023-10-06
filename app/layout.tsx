b7f868c4feafa15f8b48ef82a7418435b1cc858d
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-06 22:40:59 +0200

    tac

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="relative">{children}</body>
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
