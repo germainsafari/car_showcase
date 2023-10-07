c3f1bdfb5f8dd8f4a1aa769b0fbc214266721698
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-07 23:14:52 +0200

    tch

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+import CustomButton from "./CustomButton";
 
 function Navbar() {
   return <div>Navbar</div>;
