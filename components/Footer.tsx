6fc2067cb68206a55ba2318cad96293822466687
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-11 20:48:26 +0200

    tch

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,16 @@ const Footer = () => {
     <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
       <div className="flex md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
         <div className="flex flex-col justify-start items-start gap-6">
-          <Image src="/logo.svg" width={100} height={100} alt="Logo" />
+          <Image
+            src="/logo.svg"
+            width={188}
+            height={18}
+            alt="Logo"
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            carhub 2023 <br /> All rights reserved &copy;
+          </p>
         </div>
       </div>
     </footer>
