02b19576be36b447a6b2c400bec3a79371843ab6
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-11 20:44:53 +0200

    tch

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import Link from "next/link";
+
 const Footer = () => {
-  return;
-  <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
-    <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10"></div>
-  </footer>;
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image src="/logo.svg" width={100} height={100} alt="Logo" />
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
