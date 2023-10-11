28cdd298fc065fc88da6836ff5f145295f01c6e8
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-11 20:35:09 +0200

    tch

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-
-function Footer() {
-  return <div>Footer</div>;
-}
+import Image from "next/image";
+import Link from "next/link";
+const Footer = () => {
+  return;
+  <footer className="flex flex-col"></footer>;
+};
 
 export default Footer;
