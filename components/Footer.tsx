0fd05d4f4e8c02d4f6a50d94fc3b2400d8c5b820
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-15 15:03:22 +0200

    tch

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,9 +31,13 @@ const Footer = () => {
           </div>
 
           <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+            <p>@2023 carHub. All rights reserved</p>
             <div className="footer__copyrights-link">
               <Link href="/">
-                <a className="text-gray-500">Home</a>
+                <a className="text-gray-500">Privacy Policy</a>
+              </Link>
+              <Link href="/">
+                <a className="text-gray-500">Terms of use</a>
               </Link>
             </div>
           </div>
