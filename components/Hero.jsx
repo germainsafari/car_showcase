5b94e6168f980d06f53e3c3a43a96afae98402bd
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-15 18:26:46 +0200

    tch

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -16,28 +16,13 @@ const Hero = () => {
                 streamline your car rental experience
                 with our effortless booking process
             </p>
-            {/* remove this as well */}
-            <p className='hero__description'>
-                From compact cars to luxurious SUVs, discover a range of options tailored to meet your needs.
-            </p>
-
-
-            {/* here */}
+           
             <CustomButton
             title="Explore Cars"
             containerStyles="bg-primary-blue text-white rounded-full mt-10"
             handleClick = {handleScroll}
             />
-        {/* remove the following code */}
-        <CustomButton
-              title="Special Offers"
-              containerStyles="bg-primary-red text-white rounded-full mt-5 ml-5"
-              // Add relevant handle click action for this button
-              handleClick={() => {}}
-            />
 
-
-        {/* here */}
         </div>
         <div className='hero__image-container'>
           <div className='hero__image'>
