bd468c0026739adfb0959c1c483be90e73441b96
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-21 21:52:21 +0200

    touch

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,28 @@
+"use client";
+import Image from "next/image";
 import { Combobox, Transition } from "@headlessui/react";
 import { SearchManufacturerProps } from "@/types";
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div className="search-manufacturer">SearchManufacturer</div>;
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="Car logo"
+            />
+          </Combobox.Button>
+        </div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
