e6d9a868932c3dd823daadec30e73aceff0765bc
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-31 18:02:37 +0100

    tch

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import Image from "next/image";
 import { CustomerFilter, Hero, SearchBar } from "@/components";
-export default function Home() {
+import { fetchCars } from "@/utils";
+
+export default async function Home({ searchParams }: HomeProps) {
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || "",
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || "",
+    limit: searchParams.limit || 10,
+    model: searchParams.model || "",
+  });
   return (
     <main className="overflow-hidden">
       <Hero />
@@ -10,12 +19,16 @@ export default function Home() {
           <h1 className="text-4xl">Car Catalogue </h1>
           <p>Explore the cars you might like</p>
         </div>
-        <div className="home__filters">
-          <SearchBar />
-          <div className="home__filter-container">
-            <CustomerFilter title="fuel" />
-            <CustomerFilter title="year" />
+
+          <div className='home__filters'>
+            <SearchBar />
+
+            <div className='home__filter-container'>
+              <CustomFilter title='fuel' options={fuels} />
+              <CustomFilter title='year' options={yearsOfProduction} />
+            </div>
           </div>
+
         </div>
       </div>
     </main>
