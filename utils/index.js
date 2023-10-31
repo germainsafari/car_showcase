90e9c7e551a2408aa13071e96253f5a6bb7d7f92
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-31 17:52:49 +0100

    xxx

--- utils/index.js
+++ utils/index.js
@@ -14,3 +14,18 @@ try {
 } catch (error) {
 	console.error(error);
 }
+
+export async function fetchCars() {
+	const headers = {
+		'X-RapidAPI-Key': '488bcdbef0mshf2bf35055247fc5p16124bjsnbece7e18b98f',
+		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+}
+
+const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+	headers : headers,
+}); 
+
+const result = await response.json();
+
+return result;
