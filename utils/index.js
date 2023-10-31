fe471c7e858a3641086ce94653ab5fb1b85fbcb8
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-10-31 17:55:57 +0100

    tch

--- utils/index.js
+++ utils/index.js
@@ -1,24 +1,7 @@
-const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '488bcdbef0mshf2bf35055247fc5p16124bjsnbece7e18b98f',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
-	}
-};
-
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
-
 export async function fetchCars() {
 	const headers = {
 		'X-RapidAPI-Key': '488bcdbef0mshf2bf35055247fc5p16124bjsnbece7e18b98f',
-		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+ 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
 }
 
