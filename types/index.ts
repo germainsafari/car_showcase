d650a554e62c8131c1d9d2cbfaa394b8cf3602f5
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-11-03 19:01:50 +0100

    touch

--- types/index.ts
+++ types/index.ts
@@ -16,5 +16,53 @@ export interface SearchManufacturerProps {
 }
 
 export interface CarProps {
-    
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
+
+export interface FilterProps {
+    manufacturer?: string;
+    year?: number;
+    model?: string;
+    limit?: number;
+    fuel?: string;
+  }
+  
+export interface CustomButtonProps {
+    isDisabled?: boolean;
+    btnType?: "button" | "submit";
+    containerStyles?: string;
+    textStyles?: string;
+    title: string;
+    rightIcon?: string;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+  }
+
+export interface OptionProps {
+    title: string;
+    value: string;
+  }
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionProps[];
+  }
+
+export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+  }
+  export interface SearchManuFacturerProps {
+    manufacturer: string;
+    setManuFacturer: (manufacturer: string) => void;
+  }  
