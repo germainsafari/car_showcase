c71eadf9b822cc423bb566e14a6a30e6ce21bb29
germainsafari <pzx115866@student.wsb.poznan.pl>
2023-11-05 21:43:19 +0100

    touch

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function ShowMore() {
-  return <div>ShowMore</div>;
+  return (
+    <div className="w-full flex-center gap-5 mt-10">
+      {!isNext && (
+        <CustomButton
+          btnType="button"
+          title="Show More"
+          containerStyles="bg-primary-blue rounded-full text-white"
+          handleClick={handleNavigation}
+        />
+      )}
+    </div>
+  );
 }
 
 export default ShowMore;
