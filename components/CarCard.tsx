09c160d4b907b436ef969faeab111bb53020b53b
Safari Germain <pzx115866@student.wsb.poznan.pl>
2023-11-07 19:54:53 +0100

    message

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,10 +1,29 @@
 "use client"
 
-import React from 'react'
+import {useState} from 'react'
+import Image from 'next/image'
+import { CarProps } from '@/types'
+import  CustomButton  from './CustomButton'
 
-function CarCard({car}) {
+interface carCardProps{
+    car: CarProps;
+};
+
+const CarCard = ({ car }: carCardProps) => {
+    const {city_mpg, year, make, model, transmission, drive} = car;
   return (
-    <div>CarCard</div>
+    <div className='car-card group'>
+        <div className='car-card__content'>
+            <h2 className='car-card__content-title'>
+                {make} {model}
+            </h2>
+        </div>
+        <p >
+            <span>
+                Car Rent
+            </span>
+        </p>
+    </div>
   )
 }
 
--- types/index.ts
+++ types/index.ts
@@ -16,5 +16,16 @@ export interface SearchManufacturerProps {
 }
 
 export interface CarProps {
-    
+    city_mpg:number;
+    class:string;
+    combination_mpg:number;
+    cylinders:number;
+    displacement:number;
+    drive:string;
+    fuel_type:string;
+    highway_mpg:number;
+    make:string;
+    model:string;
+    transmission:string;
+    year:number;
 }
