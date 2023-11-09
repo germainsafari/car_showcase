16d815cc6a991b79dc9ef71a8e22a0e1a0ee6e78
Safari Germain <99614251+germainsafari@users.noreply.github.com>
2023-11-09 23:24:31 +0100

    Update SearchBar.tsx

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -8,14 +8,36 @@ const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState("");
   const handleSearch = () => {};
   return (
-    <form className="searchbar" onSubmit={handleSearch}>
-      <div className="searchbar__item">
+     <form className='searchbar' onSubmit={handleSearch}>
+      <div className='searchbar__item'>
         <SearchManufacturer
           manufacturer={manufacturer}
-          setManufacturer={setManufacturer}
+          setManuFacturer={setManuFacturer}
         />
+        <SearchButton otherClasses='sm:hidden' />
       </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+          alt='car model'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+          placeholder='Tiguan...'
+          className='searchbar__input'
+        />
+        <SearchButton otherClasses='sm:hidden' />
+      </div>
+      <SearchButton otherClasses='max-sm:hidden' />
     </form>
+  );
+};
   );
 };
 
