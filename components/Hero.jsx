31a68a595ce4f52b2bb151940a192722ce3c23f9
Safari Germain <99614251+germainsafari@users.noreply.github.com>
2023-11-10 22:09:29 +0100

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,17 @@
-
+"use client";
 
 import Image from 'next/image';
 import  CustomButton  from './CustomButton';
+
 const Hero = () => {
   const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-  }
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+  
   return (
     <div>
         <div className='flex-1 pt-36 padding-x'>
