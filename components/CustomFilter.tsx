e2128016e0df8f5591769c1bf5f40c741dfe28c5
Safari Germain <99614251+germainsafari@users.noreply.github.com>
2023-11-11 18:47:53 +0100

    Update CustomFilter.tsx

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
+import { Fragment, useState } from "react";
+import {useRouter } from "next/navigation";
+import { Listbox, Transition } from "@headlessui/react";;
+
+import { CustomFilterProps } from "@types";
+import { updateSearchParams } from "@utils";
 
 function CustomerFilter() {
-  return <div>CustomerFilter</div>;
-}
+  return (
+    <div className='w-fit'>
+    </div>;
+)}
 
 export default CustomerFilter;
